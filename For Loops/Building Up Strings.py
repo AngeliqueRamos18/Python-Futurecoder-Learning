6bc79a94e7aaca1e7fd4472fcb2cbe3ab02e650e
ChiAngel18 <angeliqueramos.dev@gmail.com>
2025-09-09 23:47:43 +0800

    Add Building Up Strings progress

--- For Loops/Building Up Strings.py
+++ For Loops/Building Up Strings.py
@@ -1,4 +1,13 @@
+
 hello = 'Hello'
 print(hello)
 hello = hello + '!'
 print(hello)
+
+# To have the program print the World each character but in opposite way
+name = 'World'
+word = ''
+for char in name:
+    word = char + word 
+    print(word)
+
